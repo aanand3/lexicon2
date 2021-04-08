ca782f09048de4c947e9b0a418b833b20be66d93
SIDNEY HALL <sidney.hall@austincc.edu>
2021-04-08 15:38:36 -0500

    Created Search Bar

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,2 +1,29 @@
 import {Component} from 'react';
-export default class Search extends Component{}
+import {Form, Button} from 'react-bootstrap'
+export default class Search extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            myWord:{},
+            userInput:''
+                };
+        this.recordUserResponse = this.recordUserResponse.bind(this)
+    }
+
+    recordUserResponse(event){
+        this.setState({userInput:event.target.value})
+    }
+
+    render(){
+        return(
+            <Form className="m-5">
+                <Form.Group controlId="formWordInput">
+                    <Form.Label>Please Input a $5 Word</Form.Label>
+                    <Form.Control type="email" onChange={this.recordUserResponse} placeholder="Enter Word"/>
+                </Form.Group>
+                    <Button onClick={() => console.log(this.state.userInput)}>Click</Button>
+            </Form>
+        );
+    }
+}
