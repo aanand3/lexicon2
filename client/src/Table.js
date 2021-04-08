a1def4901595f5e709f8a1e28850fc75f14f3838
Davon Davis <davon.davis@austincc.edu>
2021-04-08 16:53:49 -0500

    displays table

--- client/src/Table.js
+++ client/src/Table.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
 const Table = () => {
+
+    function fetchTB(){
+        //this is for the fetch request
+    }
+
+    function myTableBody(){
+        let list = [{"word": "tantamount", "price": "$5"}, 
+                    {"word": "laborious", "price": "$4"}];
+        return(
+            list.map(word =>
+            <tr>
+                <td>{word.word}</td>
+                <td>{word.price}</td>
+            </tr>
+            )
+        );
+    }
+
     return (
-        <div>
-            hello there
-        </div>
+        <table class="table table-dark table-bordered w-auto centered m-auto">
+            <thead>
+                <tr>
+                    <th scope="col">Word</th>
+                    <th scope="col">Price</th>
+                </tr>
+            </thead>
+            <tbody>{myTableBody()}</tbody>
+        </table>
     )
 }
 
